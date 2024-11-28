057d07c000d01608a7d58b10b110d0a6366d07d5
6704062617369 <s6704062617369@email.kmutnb.ac.th>
2024-11-28 18:09:50 +0700

    tell us how much different value

--- lab1minx.c
+++ lab1minx.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	int num1=100;
-	int num2=50;
-	if (num1>num2){
+	int n1;
+	int n2;
+	printf("Type a number 1: \n");
+	scanf("%d", &n1);
+	
+	printf("Type a number 2: \n");
+	scanf("%d", &n2);
+
+	if (n1>n2){
 		printf("num1 is greater than num2");
+		printf("\n%d",(n1-n2));
 	} else {
 		printf("num2 is greater than num1");
+		printf("\n%d",(n2-n1));
 	}
 }
