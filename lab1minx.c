3eadd7729b19b34b64fc7dbeb80ee6879135070c
6704062617369 <s6704062617369@email.kmutnb.ac.th>
2024-11-28 18:16:45 +0700

    n1

--- lab1minx.c
+++ lab1minx.c
@@ -3,9 +3,8 @@
 int main() {
 	int n1;
 	int n2;
-	printf("Type a number1 and number2: \n");
-	scanf("%d %d", &n1,&n2);
-
+	scanf("%d", &n1);
+	scanf("%d", &n2);
 	if (n1>n2){
 		printf("num1 is greater than num2");
 		printf("\n%d",(n1-n2));
